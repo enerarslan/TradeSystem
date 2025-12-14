f8c76d6099dad26684bdea1d471afb8f4b4acff4
Ener Arslan <enerarslan2003@hotmail.com>
2025-12-14 22:49:39 +0100

    Sİldim

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-AlphaTrade Institutional Trading System
-JPMorgan-Level Algorithmic Trading Infrastructure
-"""
-
-__version__ = "2.0.0"
-__author__ = "AlphaTrade Team"
--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-# AlphaTrade System Tests
